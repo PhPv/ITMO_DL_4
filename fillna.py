9266090f8f987c38c7f8bb42d8ab205a9c187603
PhPv <petrov.v.163@yandex.ru>
2022-11-09 18:38:17 +0300

    ОБработка исходного датасета и второй релиз

---
+++ fillna.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import datetime
+import numpy as np
+# Function to insert row in the dataframe
+def Insert_row(row_number, df, row_value):
+    # Starting value of upper half
+    start_upper = 0
+   
+    # End value of upper half
+    end_upper = row_number
+   
+    # Start value of lower half
+    start_lower = row_number
+   
+    # End value of lower half
+    end_lower = df.shape[0]
+   
+    # Create a list of upper_half index
+    upper_half = [*range(start_upper, end_upper, 1)]
+   
+    # Create a list of lower_half index
+    lower_half = [*range(start_lower, end_lower, 1)]
+   
+    # Increment the value of lower half by 1
+    lower_half = [x.__add__(1) for x in lower_half]
+   
+    # Combine the two lists
+    index_ = upper_half + lower_half
+   
+    # Update the index of the dataframe
+    df.index = index_
+   
+    # Insert a row at the end
+    df.loc[row_number] = row_value
+    
+    # Sort the index labels
+    df = df.sort_index()
+   
+    # return the dataframe
+    return df
+   
+
+from datetime import timedelta
+# добавляет в датафрейм строки с пропущенным часом и np.nan в признаках для дальнейшего заполнениях
+data_x = pd.read_csv('data.csv', sep=',')#, index_col=['dt'])
+data_x.loc[:,"dt"] = pd.to_datetime(data_x["dt"])
+count = 0
+for n in range(1, len(data_x)):
+    if data_x['dt'][n-1].hour == 10 and data_x['dt'][n].hour == 12:
+        row_number = n
+        row_value = [np.nan, (data_x['dt'][n-1]) + timedelta(hours=1),
+        np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, 
+        np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan,np.nan, data_x['dt'][n].dayofyear, ((data_x['dt'][n-1]) + timedelta(hours=1)).hour]
+        data_x = Insert_row(row_number, data_x, row_value)
+        count += 1
+
+
+        
+
+# data_x = pd.to_datetime(data_x['dt'])
+list = ['fact','10_metre_V_wind_component','Snow_density' ,'Snowfall','Visibility','Surface_pressure','Convective_precipitation',
+'Visual_cloud_cover','Total_cloud_cover','Precipitation_type','Instantaneous_10_metre_wind_gust','Medium_cloud_cover',
+'Total_precipitation_rate','Convective_available_potential_energy','10_metre_U_wind_component','Skin_temperature',
+'2_metre_temperature','Surface_solar_radiation_downwards','Wind_speed','Low_cloud_cover','Snow_depth','High_cloud_cover',
+'Evaporation','Wind_Direction','2_metre_dewpoint_temperature','Total_precipitation','2_metre_relative_humidity',
+'Clear_sky_direct_solar_radiation_at_surface','Snow_height']
+# data_x = data_x.drop(['n'], axis=1)
+for x in list:
+    # data_x[x] = data_x[x].fillna(data_x[x].median())
+    data_x[x] = data_x[x].fillna(method='ffill')
+
+# data_x.to_csv("updated_data_full.csv", index=False)
+# data_x = pd.read_csv('updated_data_full.csv', sep=',')
+del_rows = []
+for n in range(2, len(data_x)):
+    if data_x['dt'][n].hour !=  data_x['dt'][n-1].hour + 1 and data_x['dt'][n-1].hour != 23 and data_x['dt'][n] != 0:
+        p = n - 1
+        while data_x['dt'][p].hour != 23:
+            del_rows.append(data_x['dt'][p])  
+            p-=1
+
+#TODO: 12 часовые обрезки есть. 
+
+
+
+# data_y = data_x
+# for n in range(2, len(data_x)):
+#     try:
+#         if data_x['dt'][n].hour !=  data_x['dt'][n-1].hour + 1 and data_x['dt'][n-1].hour != 23:
+#             p = n - 1
+#             print(data_x['dt'][p-1])
+#             while data_x['dt'][p].hour != 23:
+#                 data_y.drop(index=(data_x['dt'][p-1]))
+#                 p-=1
+#                 print('dropped')
+            # m, l = n, 1
+            # while data_x['dt'][m-1].hour != 0:
+            #     l+=1
+            #     m-=1
+            # try:
+            #     data_x = data_x.drop(data_x.index[range(n-l,n)])
+            #     print("deleted ", l, ' ', n)
+            # except:
+            #     continue
+    # except:
+    #     print(n)
+# data_y = data_x[data_x.dt.isin(del_rows) == False]
+data_y = data_x.loc[~data_x['dt'].isin(del_rows)]  
+data_y.to_csv("updated_data_full_dropped.csv", index=False)
--- var2.py
+++ var2.py
@@ -66,125 +66,125 @@ from keras.layers import Dense
 from keras.layers import Flatten
 from keras.layers import LSTM
 
-# split a univariate dataset into train/test sets
-def split_dataset(data):
-    # split into standard weeks
-    train, test = data[1:-30*24+1], data[-30*24:-48]
-    # restructure into windows of weekly data
-    train = array(split(train, len(train)/24))
-    test = array(split(test, len(test)/24))
-    return train, test
+# # split a univariate dataset into train/test sets
+# def split_dataset(data):
+#     # split into standard weeks
+#     train, test = data[1:-30*24+1], data[-30*24:-48]
+#     # restructure into windows of weekly data
+#     train = array(split(train, len(train)/24))
+#     test = array(split(test, len(test)/24))
+#     return train, test
 
-def show_plot(true, pred, title):
-    fig = pyplot.subplots()
-    pyplot.plot(true, label='Y_original')
-    pyplot.plot(pred, dashes=[4, 3], label='Y_predicted')
-    pyplot.xlabel('N_samples', fontsize=12)
-    pyplot.ylabel('Instance_value', fontsize=12)
-    pyplot.title(title, fontsize=12)
-    pyplot.grid(True)
-    pyplot.legend(loc='upper right')
-    pyplot.show()
+# def show_plot(true, pred, title):
+#     fig = pyplot.subplots()
+#     pyplot.plot(true, label='Y_original')
+#     pyplot.plot(pred, dashes=[4, 3], label='Y_predicted')
+#     pyplot.xlabel('N_samples', fontsize=12)
+#     pyplot.ylabel('Instance_value', fontsize=12)
+#     pyplot.title(title, fontsize=12)
+#     pyplot.grid(True)
+#     pyplot.legend(loc='upper right')
+#     pyplot.show()
 
-# # evaluate one or more weekly forecasts against expected values
-def evaluate_forecasts(actual, predicted):
-    scores = list()
-    # calculate an RMSE score for each day
-    for i in range(actual.shape[1]):
-        # calculate mse
-        mse = mean_squared_error(actual[:, i], predicted[:, i])
-        # calculate rmse
-        rmse = sqrt(mse)
-        # store
-        scores.append(rmse)
-    # calculate overall RMSE
-    s = 0
-    for row in range(actual.shape[0]):
-        for col in range(actual.shape[1]):
-            s += (actual[row, col] - predicted[row, col])**2
-    score = sqrt(s / (actual.shape[0] * actual.shape[1]))
-    # plot forecasts vs observations
-    for j in range(predicted.shape[1]):
-        show_plot(actual[:, j], predicted[:, j], j + 1)
-    return score, scores
+# # # evaluate one or more weekly forecasts against expected values
+# def evaluate_forecasts(actual, predicted):
+#     scores = list()
+#     # calculate an RMSE score for each day
+#     for i in range(actual.shape[1]):
+#         # calculate mse
+#         mse = mean_squared_error(actual[:, i], predicted[:, i])
+#         # calculate rmse
+#         rmse = sqrt(mse)
+#         # store
+#         scores.append(rmse)
+#     # calculate overall RMSE
+#     s = 0
+#     for row in range(actual.shape[0]):
+#         for col in range(actual.shape[1]):
+#             s += (actual[row, col] - predicted[row, col])**2
+#     score = sqrt(s / (actual.shape[0] * actual.shape[1]))
+#     # plot forecasts vs observations
+#     for j in range(predicted.shape[1]):
+#         show_plot(actual[:, j], predicted[:, j], j + 1)
+#     return score, scores
 
-# summarize scores
-def summarize_scores(name, score, scores):
-    s_scores = ', '.join(['%.1f' % s for s in scores])
-    print('%s: [%.3f] %s' % (name, score, s_scores))
+# # summarize scores
+# def summarize_scores(name, score, scores):
+#     s_scores = ', '.join(['%.1f' % s for s in scores])
+#     print('%s: [%.3f] %s' % (name, score, s_scores))
 
-# convert history into inputs and outputs
-def to_supervised(train, n_input, n_out=24):
-    # flatten data
-    data = train.reshape((train.shape[0]*train.shape[1], train.shape[2]))
-    X, y = list(), list()
-    in_start = 0
-    # step over the entire history one time step at a time
-    for _ in range(len(data)):
-        # define the end of the input sequence
-        in_end = in_start + n_input
-        out_end = in_end + n_out
-        # ensure we have enough data for this instance
-        if out_end <= len(data):
-            x_input = data[in_start:in_end, 0]
-            x_input = x_input.reshape((len(x_input), 1))
-            X.append(x_input)
-            y.append(data[in_end:out_end, 0])
-        # move along one time step
-        in_start += 1
-    return array(X), array(y)
+# # convert history into inputs and outputs
+# def to_supervised(train, n_input, n_out=24):
+#     # flatten data
+#     data = train.reshape((train.shape[0]*train.shape[1], train.shape[2]))
+#     X, y = list(), list()
+#     in_start = 0
+#     # step over the entire history one time step at a time
+#     for _ in range(len(data)):
+#         # define the end of the input sequence
+#         in_end = in_start + n_input
+#         out_end = in_end + n_out
+#         # ensure we have enough data for this instance
+#         if out_end <= len(data):
+#             x_input = data[in_start:in_end, 0]
+#             x_input = x_input.reshape((len(x_input), 1))
+#             X.append(x_input)
+#             y.append(data[in_end:out_end, 0])
+#         # move along one time step
+#         in_start += 1
+#     return array(X), array(y)
 
-# train the model
-def build_model(train, n_input):
-    # prepare data
-    train_x, train_y = to_supervised(train, n_input)
-    # define parameters
-    verbose, epochs, batch_size = 0, 70, 16
-    n_timesteps, n_features, n_outputs = train_x.shape[1], train_x.shape[2], train_y.shape[1]
-    # define model
-    model = Sequential()
-    model.add(LSTM(200, activation='relu', input_shape=(n_timesteps, n_features)))
-    model.add(Dense(100, activation='relu'))
-    model.add(Dense(n_outputs))
-    model.compile(loss='mse', optimizer='adam')
-    # fit network
-    model.fit(train_x, train_y, epochs=epochs, batch_size=batch_size, verbose=verbose)
-    return model
+# # train the model
+# def build_model(train, n_input):
+#     # prepare data
+#     train_x, train_y = to_supervised(train, n_input)
+#     # define parameters
+#     verbose, epochs, batch_size = 0, 70, 16
+#     n_timesteps, n_features, n_outputs = train_x.shape[1], train_x.shape[2], train_y.shape[1]
+#     # define model
+#     model = Sequential()
+#     model.add(LSTM(200, activation='relu', input_shape=(n_timesteps, n_features)))
+#     model.add(Dense(100, activation='relu'))
+#     model.add(Dense(n_outputs))
+#     model.compile(loss='mse', optimizer='adam')
+#     # fit network
+#     model.fit(train_x, train_y, epochs=epochs, batch_size=batch_size, verbose=verbose)
+#     return model
 
-# make a forecast
-def forecast(model, history, n_input):
-    # flatten data
-    data = array(history)
-    data = data.reshape((data.shape[0]*data.shape[1], data.shape[2]))
-    # retrieve last observations for input data
-    input_x = data[-n_input:, 0]
-    # reshape into [1, n_input, 1]
-    input_x = input_x.reshape((1, len(input_x), 1))
-    # forecast the next week
-    yhat = model.predict(input_x, verbose=0)
-    # we only want the vector forecast
-    yhat = yhat[0]
-    return yhat
+# # make a forecast
+# def forecast(model, history, n_input):
+#     # flatten data
+#     data = array(history)
+#     data = data.reshape((data.shape[0]*data.shape[1], data.shape[2]))
+#     # retrieve last observations for input data
+#     input_x = data[-n_input:, 0]
+#     # reshape into [1, n_input, 1]
+#     input_x = input_x.reshape((1, len(input_x), 1))
+#     # forecast the next week
+#     yhat = model.predict(input_x, verbose=0)
+#     # we only want the vector forecast
+#     yhat = yhat[0]
+#     return yhat
 
-# evaluate a single model
-def evaluate_model(train, test, n_input):
-    # fit model
-    model = build_model(train, n_input)
-    # history is a list of weekly data
-    history = [x for x in train]
-    # walk-forward validation over each week
-    predictions = list()
-    for i in range(len(test)):
-        # predict the week
-        yhat_sequence = forecast(model, history, n_input)
-        # store the predictions
-        predictions.append(yhat_sequence)
-        # get real observation and add to history for predicting the next week
-        history.append(test[i, :])
-    # evaluate predictions days for each week
-    predictions = array(predictions)
-    score, scores = evaluate_forecasts(test[:, :, 0], predictions)
-    return score, scores
+# # evaluate a single model
+# def evaluate_model(train, test, n_input):
+#     # fit model
+#     model = build_model(train, n_input)
+#     # history is a list of weekly data
+#     history = [x for x in train]
+#     # walk-forward validation over each week
+#     predictions = list()
+#     for i in range(len(test)):
+#         # predict the week
+#         yhat_sequence = forecast(model, history, n_input)
+#         # store the predictions
+#         predictions.append(yhat_sequence)
+#         # get real observation and add to history for predicting the next week
+#         history.append(test[i, :])
+#     # evaluate predictions days for each week
+#     predictions = array(predictions)
+#     score, scores = evaluate_forecasts(test[:, :, 0], predictions)
+#     return score, scores
 
 import pandas as pd
 import re
@@ -216,7 +216,7 @@ print(days_hours.head())
 # удаление явно ненужных столбцов из матрицы признаков
 data = data.drop(['id', 'gtpp', 'load_time', 'predict'], axis=1)
 data = pd.concat([data, days_hours], axis=1)
-data = data.drop(data[data['fact'].values <= 10].index)
+# data = data.drop(data[data['fact'].values <= 10].index)
 # зимой станцию заметает снегом. 
 
 data = data.drop(['month'], axis=1)
@@ -225,23 +225,29 @@ data = data.drop(['month'], axis=1)
 data.replace('[^a-zA-Z0-9]', ' ', regex = True)
 data = data.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+:[C]:', '', x))
 
-# # выделение матриц признаков и целевых переменных (целевая переменная 'fact')
-# y_train = data[data.columns[0]]
-# X_train = data[data.columns[1:]]
+# выделение матриц признаков и целевых переменных (целевая переменная 'fact')
+y_train = data[data.columns[0]]
+X_train = data[data.columns[1:]]
 
-# # формирование набора для обучения и тестирования
+# формирование набора для обучения и тестирования
 # X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.01, random_state=42)
 
 
+
+
+# data.drop(data.tail(19).index,inplace=True) 
+data.to_csv('data.csv', index=False)
+
+
+
+
+
+
 # load the new file
-# dataset = read_csv('household_power_consumption_days.csv', header=0,
-#                     infer_datetime_format=True, parse_dates=['datetime'],
-#                     index_col=['datetime'])
+dataset = read_csv('household_power_consumption_days.csv', header=0,
+                    infer_datetime_format=True, parse_dates=['datetime'],
+                    index_col=['datetime'])
 # split into train and test
-
-data.drop(data.tail(19).index,inplace=True) 
-data.to_csv('data.csv')
-
 dataset = read_csv('data.csv', header=0,
                      infer_datetime_format=True, parse_dates=['dt'],
                      index_col=['dt'])
